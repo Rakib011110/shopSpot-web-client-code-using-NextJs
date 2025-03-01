a0db422466b6fba88da13172e942161d9453900d
Rakibul Islam <rakib011110@gmail.com>
2025-03-01 18:51:10 +0600

    products added

--- src/components/home/@banner/page.jsx
+++ src/components/home/@banner/page.jsx
@@ -12,7 +12,7 @@ const banners = [
   {
     id: 1,
     image:
-      "https://s3-alpha-sig.figma.com/img/dc40/ba89/7215f42e5883a64157f0aa3a4d1a866a?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=uC-sZKj3Pd53cLeHLxDv6LVF-tM2P7ObyVHE~eePoufrpFXMtCfx7aiaL-9LTl~dqnkI3UDG0JuZcfZHOojrYmN-r7wAvOBD2A3LyLNN4Xb4cLEhk6bEdo~Zwx~AAmKH8wErGRZ63rnJkScqlzh5oGt9aBGY10SA3WEYEAHxCYgTBriPiHLT0jC6iWhM7ObgKF8CGl~~MmQyZB4LKXujUpWYUgjSrI923N0oI-pTiF9IqHxG273E-85IxS6lTrij9GWyfOnk-UOLiUtYNH641HWoUiV7Aj6-2g7to04pMqNN9s6FqTZ7OTpAnkEn5fzefauir5ec52cBOFKnFFZQEQ__",
+      "https://9to5toys.com/wp-content/uploads/sites/5/2021/03/Apple-iPhone-12-Camera.jpg",
     title: "Summer Sale",
     description: "Up to 50% off on selected items",
     buttonText: "Shop Now",
@@ -20,7 +20,7 @@ const banners = [
   },
   {
     id: 2,
-    image: "/banner2.jpg",
+    image: "https://www.addictivetips.com/app/uploads/2020/11/iphone.jpg",
     title: "New Arrivals",
     description: "Discover the latest trends",
     buttonText: "Discover",
@@ -41,13 +41,8 @@ export default function Banner() {
             Fixed-size, absolutely positioned container 
             at (top=182px, left=135px, width=1187px, height=344px).
           */}
-          <div className=" top-[182px] left-[135px] w-[496px]  h-[344px]">
-            <Image
-              src={banner.image}
-              alt={banner.title}
-              fill
-              className="object-cover"
-            />
+          <div className=" top-[182px] left-[135px] w-[496px]  h-[500px]">
+            <Image src={banner.image} alt={banner.title} fill className="" />
             <div className="absolute inset-0 bg-black opacity-40" />
 
             {/* Banner Content, aligned to the left */}
