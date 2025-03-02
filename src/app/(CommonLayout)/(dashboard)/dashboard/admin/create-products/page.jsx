83960ae30a27a906138dbe2b2bb593e54b50d1ff
Rakibul Islam <rakib011110@gmail.com>
2025-03-02 22:34:16 +0600

    profile form added

--- src/app/(CommonLayout)/(dashboard)/dashboard/admin/create-products/page.jsx
+++ src/app/(CommonLayout)/(dashboard)/dashboard/admin/create-products/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CreatProducts = () => {
   return (
     <div>
-      <h1> Here will be Create Products</h1>
+      <h1> Here will be Create Products all products</h1>
     </div>
   );
 };
